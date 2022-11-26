e2ccdc91663c693b6e9cea9d2bfdad2cab5f321e
Juninho Cruz <antoniojr.ufam@gmail.com>
2022-11-26 04:34:49 -0300

    move MFEs urls to config file

--- shell/src/app.component.ts
+++ shell/src/app.component.ts
@@ -1,4 +1,5 @@
 import html from './app.component.html';
+import { mfe1Server, mfe2Server } from './config';
 
 class App extends HTMLElement {
 	creationDate: Date;
@@ -13,11 +14,11 @@ class App extends HTMLElement {
 
 		const box = document.getElementById('box')!;
 		document.getElementById('btn-mfe1')?.addEventListener('click', (event) => {
-			import(/*webpackIgnore: true*/ 'http://localhost:8081/index.bundle.js');
+			import(/*webpackIgnore: true*/ `${mfe1Server}/index.bundle.js`);
 			box.innerHTML = `<mfe1-root></mfe1-root>`;
 		});
 		document.getElementById('btn-mfe2')?.addEventListener('click', (event) => {
-			import(/*webpackIgnore: true*/ 'http://localhost:8082/index.bundle.js');
+			import(/*webpackIgnore: true*/ `${mfe2Server}/index.bundle.js`);
 			box.innerHTML = `<mfe2-root></mfe2-root>`;
 		});
 	}
---
+++ shell/src/config.ts
@@ -0,0 +1,4 @@
+const MFE_SERVER = 'http://localhost';
+
+export const mfe1Server = MFE_SERVER + ':8081';
+export const mfe2Server = MFE_SERVER + ':8082';
